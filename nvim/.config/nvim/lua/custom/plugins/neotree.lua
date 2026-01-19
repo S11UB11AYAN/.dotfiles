7d93ee860b6a0f4d3a560a9698b7a542222ff7ff
S11UB11AYAN <shubhayanbagchi30@gmail.com>
2026-01-20 01:33:45 +0530

    neotree autoupdate

--- nvim/.config/nvim/lua/custom/plugins/neotree.lua
+++ nvim/.config/nvim/lua/custom/plugins/neotree.lua
@@ -15,6 +15,11 @@ return {
 	},
 	opts = {
 		filesystem = {
+			follow_current_file = {
+				enabled = true,
+				leave_dirs_open = true, -- optional (default: false)
+			},
+			use_libuv_file_watcher = true,
 			window = {
 				mappings = {
 					["\\"] = "close_window",
